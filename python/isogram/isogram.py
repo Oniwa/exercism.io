32afb0247c430b88028c9960ef315f38da3550be
John Barksdale <jdbarks@gmail.com>
2017-08-04 20:32:35 -0500

    Initial stab at the function.  6 of 9 tests passing.

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,16 @@
-def is_isogram():
-    pass
+def is_isogram(word):
+    data = word.split()
+    letters = {}
+    isogram = True
+
+    for item in data:
+        if item in letters:
+            letters[item] += 1
+        else:
+            letters[item] = 1
+
+    for key in letters:
+        if letters[key] > 1:
+            isogram = False
+
+    return isogram
