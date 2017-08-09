44e5bb62955ffb219c8a7d19e8d03980f5fc89eb
John Barksdale <jdbarks@gmail.com>
2017-08-09 15:40:35 -0500

    All tests passing

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,14 @@
-def sieve():
-    pass
+def sieve(limit):
+    primes = list(range(2, limit + 1))
+
+    for number in primes:
+        multiple = 0
+        count = 2
+
+        while multiple < limit:
+            multiple = number * count
+            if multiple in primes:
+                primes.remove(multiple)
+            count +=1
+
+    return primes
