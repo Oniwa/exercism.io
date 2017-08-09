24cbd79ff020a66fbf5b2e56477e3094c1285415
John Barksdale <jdbarks@gmail.com>
2017-08-09 17:11:35 -0500

    Code commented

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -2,9 +2,12 @@ from string import ascii_lowercase
 
 
 def encode(plain_text):
-    plain_text = list(plain_text)
-    cypher_text = []
+    plain_text = list(plain_text)  # Listify the string
+    cypher_text = []  # Encoded cypher text
 
+    # For each item in string if it is a letter then get the encoded letter and
+    # append it to the cypher text.  If it is a number then append the number.
+    # Skip punctuation.
     for letter in plain_text:
         if letter.isalpha():
             cypher_letter_index = -1 * (ascii_lowercase.index(letter.lower()) - 25)
@@ -20,6 +23,7 @@ def grouping(cyphertext, size):
     grouped_text = []
     count = 1
 
+    # For every size (number) of letters add a space
     for item in cyphertext:
         grouped_text.append(item)
         if size % count == 0 and count != 1:
@@ -28,6 +32,7 @@ def grouping(cyphertext, size):
         else:
             count += 1
 
+    # If the last character is a space trim it
     if grouped_text[len(grouped_text) - 1] == ' ':
         grouped_text = grouped_text[:-1]
 
@@ -38,6 +43,9 @@ def decode(cypher_text):
     cypher_text = list(cypher_text)
     plain_text = []
 
+    # For each item in string if it is a letter then get the decoded letter and
+    # append it to the plain text.  If it is a number then append the number.
+    # Skip punctuation.
     for letter in cypher_text:
         if letter.isalpha():
             plain_letter_index = -1 * (ascii_lowercase.index(letter.lower()) - 25)
