3b84d277a2ce9ac8bbc37d6354edc65360e2398c
John Barksdale <jdbarks@gmail.com>
2017-08-10 14:58:03 -0500

    Commented Code

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -2,10 +2,15 @@ import re
 
 
 def abbreviate(phrase):
+    # Use the regular expression library to split the phrase up into its
+    # individual words using spaces and hyphens as the delimiter.
     phrase = re.split(' |-', phrase)
     acronym = []
 
+    # Look at each word in the phrase and take the first letter of it.
+    # Append an upper cased version of the letter to acronym list
     for word in phrase:
         acronym.append(word[0].upper())
 
+    # Turn the list into a string and return
     return ''.join(acronym)
