70880609faa9c15679995335af72f39ab8f7e3ef
John Barksdale <jdbarks@gmail.com>
2017-08-10 14:28:12 -0500

    All tests passing

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,10 @@
-def sum_of_multiples():
-    pass
+def sum_of_multiples(limit, multiples):
+    summation = 0
+
+    for number in range(1, limit):
+        for multiple in multiples:
+            if number % multiple == 0:
+                summation += number
+                break
+
+    return summation
