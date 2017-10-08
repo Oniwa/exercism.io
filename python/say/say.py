974474453b698439c1b43fd5ecd370ed1e096936
John Barksdale <jdbarks@gmail.com>
2017-10-08 16:53:11 -0500

    Changed my approach to this problem.  I now have only 7 tests failing.  Removed unused code and refactored.

--- python/say/say.py
+++ python/say/say.py
@@ -32,26 +32,34 @@ teens = {10: 'ten',
          19: 'nineteen'
          }
 
+power_3 = {0: '',
+           1: 'thousand',
+           2: 'million and',
+           3: 'billion and'
+           }
 
 def say(number):
+    number = int(number)
+    number_to_say = number_splitter(number)
+    number_of_digits = len(str(number))
+    verbal_number = []
+    count = 0
+    if number_of_digits == 1 and number_to_say == [0]:
+        return ones[0]
+    for item in reversed(number_to_say):
+        verbal_number.append(say_number(item, count))
+        count += 1
+
+    return ' '.join(reversed(verbal_number))
+
+
+def say_number(number, count):
     number_digits = str(number)
     verbal_number = None
 
-    if 0 <= number < 100:
-        verbal_number = say_two_digits(number)
-    elif 100 <= number < 1000:
-        verbal_number = say_three_digits(number)
-    elif 1000<= number < 10000:
-        verbal_number = say_four_digits(number)
-
-    return verbal_number
-
-
-def say_two_digits(number):
-    number_digits = str(number)
-    verbal_number = None
-
-    if 0 <= number < 10:
+    if number == 0:
+        verbal_number = power_3[count + 1]
+    elif 0 <= number < 10:
         verbal_number = ones[number]
     elif 10 <= number < 20:
         verbal_number = teens[number]
@@ -61,34 +69,15 @@ def say_two_digits(number):
                                            ones[int(number_digits[1])])
         else:
             verbal_number = tens[int(number_digits[0])]
-
-    return verbal_number
-
-
-def say_three_digits(number):
-    number_digits = str(number)
-
-    if int(number_digits[1]) == int(number_digits[2]) == 0:
-        verbal_number = "{} hundred".format(ones[int(number_digits[0])])
-    else:
-        two_digits = '{}{}'.format(number_digits[1], number_digits[2])
-        two_digits = int(two_digits)
-        verbal_number = "{} hundred and {}".format(ones[int(number_digits[0])],
-                                                   say_two_digits(two_digits))
-
-    return verbal_number
-
-
-def say_four_digits(number):
-    number_digits = str(number)
-
-    if int(number_digits[1]) == int(number_digits[2]) == \
-            int(number_digits[3]) ==  0:
-        verbal_number = "{} thousand".format(ones[int(number_digits[0])])
-    else:
-        three_digits = int(number_digits[-3:])
-        verbal_number = "{} thousand {}".format(ones[int(number_digits[0])],
-                                                    say_three_digits(three_digits))
+    elif 100 <= number < 1000:
+        if int(number_digits[1]) == int(number_digits[2]) == 0:
+            verbal_number = "{} hundred{}".format(ones[int(number_digits[0])],
+                                           power_3[count])
+        else:
+            two_digits = '{}{}'.format(number_digits[1], number_digits[2])
+            two_digits = int(two_digits)
+            verbal_number = "{} hundred and {}".format(ones[int(number_digits[0])],
+                                                       say_number(two_digits, 0))
 
     return verbal_number
 
