5c44797078c519d0ab27b9eaa4ad06dc0cf4b3f1
John Barksdale <jdbarks@gmail.com>
2017-10-12 17:34:32 -0500

    Finished with all tests passing

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,3 +1,26 @@
 class School(object):
     def __init__(self, name):
-        pass
+        self.grades = {1: [],
+                       2: [],
+                       3: [],
+                       4: [],
+                       5: [],
+                       6: [],
+                       7: [],
+                       8: [],
+                       9: [],
+                       }
+
+    def add(self, name, grade_number):
+        self.grades[grade_number].append(name)
+
+    def grade(self, grade_number):
+        return self.grades[grade_number]
+
+    def sort(self):
+        sorted_list = []
+        for _ in range(1, 10):
+            if self.grades[_]:
+                sorted_list.append((_, tuple(sorted(self.grades[_]))))
+
+        return sorted_list
