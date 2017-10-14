647bb7dceb6fa3e80965bffaf38d8daae6f22912
John Barksdale <jdbarks@gmail.com>
2017-10-13 23:41:40 -0500

    All tests pass

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,23 @@
 def flatten(iterable):
-    pass
+    flattened_list = []
+
+    flat = flattener(iterable)
+
+    for item in flat:
+        flattened_list.append(item)
+
+    return flattened_list
+
+def flattener(iterable):
+    flattened = []
+
+    for item in iterable:
+        if type(item) is list:
+            flat = flattener(item)
+            for thing in flat:
+                flattened.append(thing)
+        else:
+            if item or item == 0:
+                flattened.append(item)
+
+    return flattened
